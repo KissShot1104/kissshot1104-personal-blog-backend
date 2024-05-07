0ab7bbc0e95991ee6d5ee6f0f2f76c9c9c9fd093
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-05-08 06:09:33 +0900

    feat: Service에 모든 Category를 tree형태로 조회하는 메서드 추가

--- src/main/java/kissshot1104/personal/blog/category/service/CategoryService.java
+++ src/main/java/kissshot1104/personal/blog/category/service/CategoryService.java
@@ -1,7 +1,51 @@
 package kissshot1104.personal.blog.category.service;
 
+
+import kissshot1104.personal.blog.category.dto.response.FindCategoryResponse;
+import kissshot1104.personal.blog.category.entity.Category;
+import kissshot1104.personal.blog.category.repository.CategoryRepository;
+import kissshot1104.personal.blog.global.exception.BusinessException;
+import kissshot1104.personal.blog.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    //todo 리팩토링 해야함 메모
+    public List<FindCategoryResponse> findAllCategory() {
+        final List<Category> categoryList = categoryRepository.findAll();
+        Map<Long, FindCategoryResponse> findCategoryResponseMap = new HashMap<>();
+
+        categoryList.forEach(category ->
+                findCategoryResponseMap.put(category.getId(),
+                        FindCategoryResponse.of(category.getId(), category.getCategoryDepth(), category.getCategoryName()))
+                );
+
+        categoryList.forEach(category -> {
+            if (category.getCategory() != null) {
+                findCategoryResponseMap.get(category.getCategory().getId()).addChild(findCategoryResponseMap.get(category.getId()));
+            }
+        });
+
+        final List<FindCategoryResponse> findCategoryResponseList =
+                findCategoryResponseMap.values().stream()
+                        .filter(FindCategoryResponse -> FindCategoryResponse.categoryDepth() == 0)
+                        .toList();
+        return findCategoryResponseList;
+    }
+
+    public Category findByCategoryId(final Long categoryId) {
+        final Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND));
+        return category;
+    }
+
 }
