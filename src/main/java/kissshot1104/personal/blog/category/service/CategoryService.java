7b27ae6c84ae795c7dde9c98492faebd99bc7d27
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-05-08 09:33:07 +0900

    feat: Service에 category추가 메서드 추가

--- src/main/java/kissshot1104/personal/blog/category/service/CategoryService.java
+++ src/main/java/kissshot1104/personal/blog/category/service/CategoryService.java
@@ -1,23 +1,54 @@
 package kissshot1104.personal.blog.category.service;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import kissshot1104.personal.blog.category.dto.request.CreateCategoryRequest;
 import kissshot1104.personal.blog.category.dto.response.FindCategoryResponse;
 import kissshot1104.personal.blog.category.entity.Category;
 import kissshot1104.personal.blog.category.repository.CategoryRepository;
 import kissshot1104.personal.blog.global.exception.BusinessException;
 import kissshot1104.personal.blog.global.exception.ErrorCode;
+import kissshot1104.personal.blog.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public Category createCategory(final CreateCategoryRequest request, final Member member) {
+        final Category parentCategory = fetchParentCategory(request.parentCategoryId());
+        return createCategoryInternal(request, parentCategory);
+    }
+
+    private Category fetchParentCategory(final Long parentCategoryId) {
+        if (parentCategoryId == null) {
+            return null;
+        }
+
+        final Category parentCategory = categoryRepository.findById(parentCategoryId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
+
+        return parentCategory;
+    }
+
+    private Category createCategoryInternal(final CreateCategoryRequest request, final Category parent) {
+        //최상위 카테고리는 0 그 밑으로 내려갈수록 1씩 증가함
+        final long depth = (parent == null) ? 0 : parent.getCategoryDepth() + 1;
+        final Category category = Category.builder()
+                .category(parent)
+                .categoryName(request.categoryName())
+                .categoryDepth(depth)
+                .build();
+
+        final Category savedCategory = categoryRepository.save(category);
+        return savedCategory;
+    }
 
     //todo 리팩토링 해야함 메모
     public List<FindCategoryResponse> findAllCategory() {
@@ -27,7 +58,7 @@ public class CategoryService {
         categoryList.forEach(category ->
                 findCategoryResponseMap.put(category.getId(),
                         FindCategoryResponse.of(category.getId(), category.getCategoryDepth(), category.getCategoryName()))
-                );
+        );
 
         categoryList.forEach(category -> {
             if (category.getCategory() != null) {
