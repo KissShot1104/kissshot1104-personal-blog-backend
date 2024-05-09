8c8ba99271ac75a00b9861024a180b41eea0ccb0
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-05-10 03:07:20 +0900

    feat: controller에서 saveCategoryChanges 메서드에서 사용할 dto 추가

---
+++ src/main/java/kissshot1104/personal/blog/category/dto/ModifyCategoryDto.java
@@ -0,0 +1,27 @@
+package kissshot1104.personal.blog.category.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+import kissshot1104.personal.blog.category.dto.request.ModifyCategoryRequest;
+import lombok.Builder;
+
+@Builder
+public record ModifyCategoryDto(Long categoryId,
+                                @NotBlank(message = "카테고리 이름은 필수입니다.") String categoryName,
+                                Long parentCategoryId) {
+    public ModifyCategoryRequest toCategoryRequest() {
+        final ModifyCategoryRequest modifyCategoryRequest = ModifyCategoryRequest.builder()
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .parentCategoryId(parentCategoryId)
+                .build();
+        return modifyCategoryRequest;
+    }
+
+    public static List<ModifyCategoryRequest> toModifyCategoryRequests(final List<ModifyCategoryDto> modifyCategoryDtos) {
+        final List<ModifyCategoryRequest> modifyCategoryRequests = modifyCategoryDtos.stream()
+                .map(ModifyCategoryDto::toCategoryRequest)
+                .toList();
+        return modifyCategoryRequests;
+    }
+}
