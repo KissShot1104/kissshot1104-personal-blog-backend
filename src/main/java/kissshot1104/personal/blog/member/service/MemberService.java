1919a012f07b1a1a029149f778375ece19773fb2
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-05-28 17:35:26 +0900

    feat: MemberService에 동일한 사용자인지 검사하는 로직 추가

--- src/main/java/kissshot1104/personal/blog/member/service/MemberService.java
+++ src/main/java/kissshot1104/personal/blog/member/service/MemberService.java
@@ -1,7 +1,19 @@
 package kissshot1104.personal.blog.member.service;
 
+import kissshot1104.personal.blog.global.exception.AuthException;
+import kissshot1104.personal.blog.global.exception.BusinessException;
+import kissshot1104.personal.blog.global.exception.ErrorCode;
+import kissshot1104.personal.blog.member.entity.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+    public void checkAuthorizedMember(final Member author, final Member requester) {
+        if (author == null || requester == null) {
+            throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+        if (author != requester) {
+            throw new AuthException(ErrorCode.UNAUTHORIZED_USER);
+        }
+    }
 }
