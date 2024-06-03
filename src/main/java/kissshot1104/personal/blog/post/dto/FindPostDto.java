f653c29736f7fd1a8f3f4706d8d58af3135df4be
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-06-04 06:56:24 +0900

    feat(PostController): 다수의 게시글을 조회하는 메서드 추가

--- src/main/java/kissshot1104/personal/blog/post/dto/FindPostDto.java
+++ src/main/java/kissshot1104/personal/blog/post/dto/FindPostDto.java
@@ -1,7 +1,9 @@
 package kissshot1104.personal.blog.post.dto;
 
+import java.util.List;
 import kissshot1104.personal.blog.post.dto.response.FindPostResponse;
 import lombok.Builder;
+import org.springframework.data.domain.Page;
 
 @Builder
 public record FindPostDto(Long postId,
@@ -21,5 +23,11 @@ public record FindPostDto(Long postId,
                 .postSecurity(response.getPostSecurity())
                 .build();
         return findPostDto;
+    }
+
+    public static Page<FindPostDto> listOf(final Page<FindPostResponse> responses) {
+        final Page<FindPostDto> findPostDtos = responses
+                .map(FindPostDto::of);
+        return findPostDtos;
     }
 }
