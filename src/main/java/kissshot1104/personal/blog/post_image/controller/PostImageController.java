cfac6ecca3f5badabeb1260965780a4e0201fd56
KissShot1104 <kissshot.heartunderblade1104@gmail.com>
2024-06-06 02:44:10 +0900

    feat(MemberController): 프로필 이미지 변경 메서드 및 프로필 이미지 출력 메서드 추가

--- src/main/java/kissshot1104/personal/blog/post_image/controller/PostImageController.java
+++ src/main/java/kissshot1104/personal/blog/post_image/controller/PostImageController.java
@@ -6,7 +6,7 @@ import kissshot1104.personal.blog.post_image.service.PostImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,9 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class PostImageController {
 
     private final PostImageService postImageService;
+
     @PostMapping("/upload-image")
-    public ResponseEntity<String> savePostImage(@RequestParam(value = "image") MultipartFile image,
-                                             @CurrentMember Member member) {
+    public ResponseEntity<String> savePostImage(@RequestPart("image") MultipartFile image,
+                                                @CurrentMember Member member) {
         String imagePath = postImageService.savePostImage(image, member);
         return ResponseEntity.ok(imagePath);
     }
